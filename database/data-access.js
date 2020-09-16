19c3ce1c434acaff897c887b5830414dd0d824cd
Brendan <bperry@hey.com>
2020-09-16 16:18:32 -0400

    working on pipeline

--- database/data-access.js
+++ database/data-access.js
@@ -8,16 +8,16 @@ module.exports = {
         // I then used the command db to see my current database which was 'test'
         // then I did db.users.insertOne({ name: 'Bryan' }) to make a new object user and insert bryan
 
-        MongoClient.connect('mongodb://localhost:27017/users', function (err, database) {
-        if (err) console.log(err)
+        // MongoClient.connect('mongodb://localhost:27017/users', function (err, database) {
+        // if (err) console.log(err)
 
-        const db = database.db('test')
+        // const db = database.db('test')
 
-        db.collection('users').find().toArray(function (err, result) {
-            if (err) console.log(err)
+        // db.collection('users').find().toArray(function (err, result) {
+        //     if (err) console.log(err)
 
-            console.log(result)
-        })
-        })
+        //     console.log(result)
+        // })
+        // })
     }
 }
