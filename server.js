7e7e9d8e79038f72063bed1d3d8ed010b1635c79
Brendan Perry <perrbm04@pfw.edu>
2020-11-17 16:35:09 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const inputFolder = './public/images/';
 const processedFolder = './public/compressed-images/';
 const fs = require('fs');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
